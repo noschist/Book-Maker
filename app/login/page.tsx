1767c7790587f30419289c111518cad35f557f39
noschist <sreenair1999@gmail.com>
2024-06-15 22:48:35 +0530

    tauri config to prevent back and forward swipe

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 import LoginScreen from "@/components/login/LoginScreen";
 
 export default function LoginPage() {
-  return <main className="overscroll-none overflow-auto h-full">
+  return <main>
     <LoginScreen />
   </main>
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use webview2_com::Microsoft::Web::WebView2::Win32::ICoreWebView2Settings6;
+use windows::core::Interface;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            window.with_webview(|webview| unsafe {
+                let settings = webview.controller().CoreWebView2().unwrap().Settings().unwrap();
+                let settings = settings.cast::<ICoreWebView2Settings6>().unwrap();
+                settings.SetIsSwipeNavigationEnabled(false).unwrap();
+            }).unwrap();
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
