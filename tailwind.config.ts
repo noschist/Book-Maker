9c105764bcdb38b5c1c8bd0d1f0fa945808726e9
noschist <sreenair1999@gmail.com>
2024-06-21 18:17:16 +0530

    chnaging form ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,10 @@ const config: Config = {
         inter: ['var(--font-inter)'],
         quicksand: ['var(--font-quicksand)'],
         playnl: ['var(--font-playwritenl)']
-      }
+      },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
     },
   },
   plugins: [addVariablesForColors],
